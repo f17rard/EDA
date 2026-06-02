f0a01d4f11e5e495982d931bb1d2b257e19e560b
Gabriel Meneses <gabomendua@gmail.com>
2026-06-02 07:33:10 -0600

    proyecto Guia 3

---
+++ EjerciciosGuia1/Ejercicio3/Program.cs
@@ -0,0 +1,21 @@
+﻿list<int> ListA = new list<int>
+ListA.Add(1);
+ListA.Add(2);
+ListA.Add(3);
+ListA.Add(4);
+ListA.Add(5);
+
+static void PrintList(List<int> list, string label)
+{
+    Console.Write(label + ": [ ");
+
+    for(int i=0;  i<list.Count; i++)
+    {
+        Console.Write(list[i]);
+        if(i < list.Count - 1) Console.Write(", ");
+    }
+
+    Console.Write(" ]");
+}
+
+
